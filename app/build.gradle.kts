3a06714d9a0b3d1223a9f9026aab4da7ea3943b9
manzolo <manzolo@geojournal.local>
2026-03-23 21:48:58 +0100

    chore: bump versionCode=75, versionName=0.4.47

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "it.manzolo.geojournal"
         minSdk = 26
         targetSdk = 36
-        versionCode = 74
-        versionName = "0.4.46"
+        versionCode = 75
+        versionName = "0.4.47"
         
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
